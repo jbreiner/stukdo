d95043b5fe6c36ee98eac5297a0848764d435c6a
John Breiner <jbreiner@gmail.com>
2015-01-24 17:10:55 -0600

    Added a user id to taks, configured user task associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124124412) do
+ActiveRecord::Schema.define(version: 20150124225046) do
 
   create_table "tasks", force: true do |t|
     t.string   "content"
     t.string   "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
